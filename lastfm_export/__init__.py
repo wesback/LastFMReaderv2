54c849eb36275372c365ddceecfed9556f3a407d
Wesley Backelant <wesback@users.noreply.github.com>
2026-09-18 15:04:23 +0200

    Implement conservative clean-title normalization and annotation rules (#32)

--- lastfm_export/__init__.py
+++ lastfm_export/__init__.py
@@ -1,3 +1,7 @@
 """Installable Last.fm scrobble exporter package."""
 
+from .titles import DEFAULT_ANNOTATION_KEYWORDS, clean_title
+
 __version__ = "0.1.0"
+
+__all__ = ["DEFAULT_ANNOTATION_KEYWORDS", "__version__", "clean_title"]
